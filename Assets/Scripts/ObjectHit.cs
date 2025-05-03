12649a09119af9967342a8fd813bdb5860a18985
Louise Collins <louisecollins1971@icloud.com>
2025-05-03 16:00:29 +0100

    Using GetComponent()

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,6 +4,7 @@ public class ObjectHit : MonoBehaviour
 {
     private void OnCollisionEnter(Collision other)
     {
+        GetComponent<MeshRenderer>().material.color = Color.red;
         Debug.Log("Something hit me!");
     }
 }
