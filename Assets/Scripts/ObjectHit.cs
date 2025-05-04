8df8fd53f852b5c6046ed1ab3800008ae86e7fd9
Louise Collins <louisecollins1971@icloud.com>
2025-05-04 23:15:16 +0100

    Using tags problem solved with dropping object

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
+    private void OnCollisionEnter(Collision other)
+    {
+         if (other.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;   
+        }  
+    }
+    
     private void OnCollisionExit(Collision other)
     {
         if (other.gameObject.tag == "Player")
-        {
-            GetComponent<MeshRenderer>().material.color = Color.red;
+        {  
             gameObject.tag = "Hit";
-        }
-       
+        }  
     }
 }
