070aec5503ff9a6fc75b4ec14f381a7a05dd17a2
Louise Collins <louisecollins1971@icloud.com>
2025-05-06 03:50:37 +0100

    Unity Order of Execution

--- Assets/Scripts/TriggerProjectile.cs
+++ Assets/Scripts/TriggerProjectile.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class TriggerProjectile : MonoBehaviour
 {
     [SerializeField] GameObject[] gameObjects;
-    
+
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
