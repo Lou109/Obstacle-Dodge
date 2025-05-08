80c72dd433feab40da6736410c22ae32efb5f3f6
Louise Collins <louisecollins1971@icloud.com>
2025-05-08 15:17:04 +0100

    lots of tweaking

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
@@ -5,21 +6,54 @@ public class Mover : MonoBehaviour
     
     [SerializeField] float speed;
     [SerializeField] float rotationSpeed;
+    [SerializeField] float jumph;
+    [SerializeField] float jumpforce;
+    Vector3 jump;
+    Rigidbody myRigidbody;
+    bool isgrounded;
+
+    void Start()
+    {
+        jump = new Vector3(0f, jumph, 0f);
+        myRigidbody = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        MovePlayer();
+        PlayerJump();
 
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        movementDirection.Normalize();
-
-        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
-        
-        if (movementDirection != Vector3.zero)
+        void MovePlayer()
         {
-            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);       
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+            Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+            movementDirection.Normalize();
+
+            transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+
+            if (movementDirection != Vector3.zero)
+            {
+                Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            }
+        }
+        void PlayerJump()
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && isgrounded)
+            {
+                myRigidbody.AddForce(jump * jumpforce, ForceMode.Impulse);
+                isgrounded = false;
+            }
         }
     }
-}
+            void OnCollisionEnter(Collision other) 
+            {
+                if(other.gameObject.tag == "Ground")
+                isgrounded = true;
+            }
+            
+               
+        }
+    
