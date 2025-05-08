8cf7aa302c6ccc6eab456cbbf8e7ce7e221c3d0a
Louise Collins <louisecollins1971@icloud.com>
2025-05-08 13:24:00 +0100

    update progression on Character control behavior

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,59 +3,23 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     
-    [SerializeField] float moveSpeed = 5f;
-    [SerializeField] float rotationSpeed = 300f;
-    [SerializeField] float jumpSpeed = 10f;
+    [SerializeField] float speed;
+    [SerializeField] float rotationSpeed;
 
-    CharacterController characterController;
-    float ySpeed;
-    float originalStepOffset;
-
-    void Start()
-    {
-        characterController = GetComponent<CharacterController>();
-        originalStepOffset = characterController.stepOffset;
-    }
-  
     void Update()
     {
-        MovePlayer();
-    }
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-    void MovePlayer()
-    {
-       float horizontalInput = Input.GetAxis("Horizontal");
-       float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection.Normalize();
 
-       Vector3 movementDirection = new Vector3(horizontalInput, 0 , verticalInput);
-       float magnitude = Mathf.Clamp01(movementDirection.magnitude) * moveSpeed;
-       movementDirection.Normalize();
-
-       ySpeed += Physics.gravity.y * Time.deltaTime;
-
-       if (characterController.isGrounded)
-       {    
-            characterController.stepOffset = originalStepOffset;
-            ySpeed = -0.5f;
-
-            if (Input.GetButtonDown("Jump"))
-            {
-                ySpeed = jumpSpeed;
-            }
-       }
-       else
-       {
-            characterController.stepOffset = 0;
-       }
-        Vector3 velocity = movementDirection * magnitude;
-        velocity.y = ySpeed;
-
-        characterController.Move(velocity * Time.deltaTime);
-
-       if (movementDirection != Vector3.zero)
-       {
-           Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-           transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-       }
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+        
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);       
+        }
     }
 }
