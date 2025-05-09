a4313076a80b75422dd4f7ebdffce2190c1e4e5e
Louise Collins <louisecollins1971@icloud.com>
2025-05-09 05:50:42 +0100

    working on Animation and movement

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{   
+    [SerializeField] bool isRunning = false;
+    Animator animator;
+    Mover mover;
+ 
+    void Start()
+    {
+        mover = GetComponent<Mover>();
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        
+        {
+           Debug.LogError("Animator component not found on this game object.");
+           enabled = false;
+           return;
+        }
+    }
+
+    void Update()
+    {
+        UpdateAnimationParameters();
+
+    }
+
+    private void UpdateAnimationParameters()
+    {
+        bool isMoving = Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0;
+
+        animator.SetBool("isMoving", isMoving);
+
+        if (Input.GetKey(KeyCode.LeftShift)) 
+        {
+            animator.SetBool("isRunning", isRunning);
+            
+        }
+        else
+        {
+            animator.SetBool("isMoving", isMoving);
+
+        }
+       
+    }
+}
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,45 +1,56 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Mover : MonoBehaviour
 {
-    
+
     [SerializeField] float speed;
     [SerializeField] float rotationSpeed;
     [SerializeField] float jumph;
     [SerializeField] float jumpforce;
+    [SerializeField] float runSpeed;
+    [SerializeField] float walkSpeed;
+
     Vector3 jump;
     Rigidbody myRigidbody;
+
     bool isgrounded;
+    bool walking = true;
+    bool isRunning = false;
 
     void Start()
     {
         jump = new Vector3(0f, jumph, 0f);
         myRigidbody = GetComponent<Rigidbody>();
+       
     }
 
     void Update()
     {
         MovePlayer();
         PlayerJump();
+        PlayerRun();
 
-        void MovePlayer()
+    void MovePlayer()
         {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-            Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-            movementDirection.Normalize();
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection.Normalize();
 
-            transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
 
             if (movementDirection != Vector3.zero)
             {
                 Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+                transform.rotation = Quaternion.RotateTowards
+                (transform.rotation, toRotation, rotationSpeed * Time.deltaTime);    
             }
+            GetComponent<Animator>().SetFloat("Speed", speed);
         }
-        void PlayerJump()
+    void PlayerJump()
         {
             if (Input.GetKeyDown(KeyCode.Space) && isgrounded)
             {
@@ -48,12 +59,36 @@ public class Mover : MonoBehaviour
             }
         }
     }
-            void OnCollisionEnter(Collision other) 
+    void OnCollisionEnter(Collision other) 
             {
                 if(other.gameObject.tag == "Ground")
                 isgrounded = true;
             }
+
+    void PlayerRun()
+    {
+            if (Input.GetKey(KeyCode.LeftShift)) 
+            {
+                if (!isRunning)
+            {
+                isRunning = true;
+                speed = runSpeed;
+            }
+                else {
+                
+                if (isRunning) 
+                {
+                    isRunning = false;
+                    speed = walkSpeed;
+                }
+            }
+        }   
+    }
             
-               
-        }
+}          
+         
+
+
+    
+
     
